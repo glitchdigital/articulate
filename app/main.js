57691b8eedcb6bd5db2daf1a1ad737e726db58ef
Iain Collins <me@iaincollins.com>
2016-06-13 18:34:49 +0100

    Localized all dependanies, resizable panes
    Localized all dependanies (no remote includes) and added a HTML text editor based on the Bootstrap version of WYSIHTML5.
    
    This is an interim measure I intend to replace it with a different, better supported editor (as fixing all the old resizing, auto scrolling and formatting bugs is a rabbit hole I want to avoid) but it's serviceable enough to use to build out the other functionality.

--- app/main.js
+++ app/main.js
@@ -4,22 +4,41 @@ const {app} = electron;
 // Module to create native browser window.
 const {BrowserWindow} = electron;
 
+var express = require('express'),
+    partials = require('express-partials'),
+    webapp = express(),
+    ejs = require('ejs');
+    
+webapp.set('view engine', 'ejs');
+webapp.engine('ejs', ejs.__express);
+partials.register('.ejs', ejs);
+webapp.use(partials());
+webapp.use(express.static(__dirname));
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
 function createWindow() {
+  console.log("new window");
   // Create the browser window.
   win = new BrowserWindow({width: 1200, height: 900});
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`);
-
+  //win.loadURL(`file://${__dirname}/index.html`);
+  
   // Open the DevTools.
   //win.webContents.openDevTools();
 
+  // Server server on automatically assigned port
+  var listener = webapp.listen(0, function(s) {
+    console.log(listener.address().port);
+    console.log('Server running at http://127.0.0.1:%d', listener.address().port);
+    win.loadURL('http://127.0.0.1:'+listener.address().port+'/index.html');
+  });
+  
   // Emitted when the window is closed.
   win.on('closed', () => {
+    listener.close();
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
